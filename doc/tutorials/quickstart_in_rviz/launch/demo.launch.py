c2d678f69001a77da86e8f62a34ca517037d081e
rnangojam <hyeokjo9707@daum.net>
2024-05-25 01:24:07 +0900

    asdf

--- doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
+++ doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
@@ -87,7 +87,7 @@ def launch_setup(context, *args, **kwargs):
         output="both",
         parameters=[moveit_config.robot_description],
     )
-
+    
     # ros2_control using FakeSystem as hardware
     ros2_controllers_path = os.path.join(
         get_package_share_directory("moveit_resources_panda_moveit_config"),
